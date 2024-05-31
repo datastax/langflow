11bd82693b9bb322ac91a4dd0c0fcb6f32e14dce
ming luo <itestmycode@gmail.com>
2024-05-31 18:45:36 -0400

    build backend with endpoint entry

--- ragstack/docker/build-backend.sh
+++ ragstack/docker/build-backend.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
 RAGSTACK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
+
+cd $RAGSTACK_DIR
+version=$(poetry version | awk '{print $2}')
+echo "build docker image version $version ..."
+
 cd $RAGSTACK_DIR/docker/backend
 echo "Building backend image"
-docker build -t ragstack-ai-langflow-backend:latest -f Dockerfile ../../..
+docker build --build-arg VERSION=${version} -t ragstack-ai-langflow-backend:latest -f Dockerfile ../../..
 echo "Done ragstack-ai-langflow-backend:latest "
+
+cd $RAGSTACK_DIR/docker/backend-ep
+docker build --build-arg VERSION=${version} -t ragstack-ai-langflow-backend-ep:latest -f Dockerfile ../../..
+echo "Done ragstack-ai-langflow-backend-ep:latest "
+
